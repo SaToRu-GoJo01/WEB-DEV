c26a4a68eddf27a58dd9af26ad2523a227ff653e
Arnav Chaudhary <arnavchaudhary63@gmail.com>
2024-05-21 23:00:28 +0530

    Day-2.0

---
+++ JS/Arrays.js
@@ -0,0 +1,7 @@
+let foods = ["biryani","kulfi","fried Rice"];
+console.log(foods);
+console.log(foods[0]);
+
+let str = foods.join(",");
+console.log(str);
+console.log(str.split(","));
--- JS/string.js
+++ JS/string.js
@@ -3,3 +3,11 @@ let color = colors.split(" ");
 console.log(colors);
 console.log(color);
 console.log(color, typeof colors);
+
+
+let str = "22";
+let num = Number(str);
+console.log(typeof num,typeof str);
+let str2 = "Hello";
+let num2 = Number(str2);
+console.log(typeof Number(str2),num2);
