9d67d8c5ac2631e82510fc0c31d2576b56119948
Arnav Chaudhary <arnavchaudhary63@gmail.com>
2024-05-22 11:49:06 +0530

    Day-2.0.1

--- JS/Arrays.js
+++ JS/Arrays.js
@@ -5,3 +5,10 @@ console.log(foods[0]);
 let str = foods.join(",");
 console.log(str);
 console.log(str.split(","));
+
+let dishes = ["Naan","Chole","Bhature","Paneer",];
+let newDishes = ["Chapati","Dal-Makhani"];
+console.log(dishes.concat(newDishes));
+console.log(newDishes.push("Puri"));//Push()-Returns the new length of the array.
+console.log(newDishes.pop());
+console.log(newDishes);
